8cd4ea982972d9314320aa82746d03de83a87b4c
Gusti Henry <g.ramahenry2439@gmail.com>
2022-05-22 23:30:54 -0400

    moved stuff around to start building

---
+++ scripts/tweepy_data.py
@@ -0,0 +1,12 @@
+import tweepy
+import numpy
+import random as rd 
+import config
+
+client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+
+def getRandomTweet():
+    
+
+if __name__ == "__main__":
+    print(rd.randrange(1,10))
